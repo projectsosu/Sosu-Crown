18c9009b84dd8ec940178cd7442908180a3b71e6
ogzkhrmn <ogz.khrmn52@gmail.com>
2020-06-16 11:38:24 +0300

    Changed wrong entity name

--- src/main/java/com/sosu/rest/crown/controller/impl/ProductControllerImpl.java
+++ src/main/java/com/sosu/rest/crown/controller/impl/ProductControllerImpl.java
@@ -3,6 +3,8 @@ package com.sosu.rest.crown.controller.impl;
 import com.sosu.rest.crown.controller.ProductController;
 import com.sosu.rest.crown.entity.postgres.Product;
 import com.sosu.rest.crown.service.ProductService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/java")
 public class ProductControllerImpl implements ProductController {
+
+    Logger logger = LogManager.getLogger(ProductController.class);
 
     @Autowired
     private ProductService productService;
@@ -23,6 +27,7 @@ public class ProductControllerImpl implements ProductController {
 
     @Override
     public List<Product> getProductByCategory(String category_id, int pageSize, String sortBy, boolean desc) {
+        logger.info("Request: {} {} {} {}", category_id, pageSize, sortBy, desc);
         return productService.getProductByCategory(category_id, pageSize, sortBy, desc);
     }
 }
