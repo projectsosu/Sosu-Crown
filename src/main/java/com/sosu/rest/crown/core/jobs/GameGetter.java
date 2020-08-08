016689f9f9f89acc87f8f31b1d9d13acb312f675
ogzkhrmn <ogz.khrmn52@gmail.com>
2020-08-09 00:14:04 +0300

    Added parent id category getter

--- src/main/java/com/sosu/rest/crown/core/jobs/GameGetter.java
+++ src/main/java/com/sosu/rest/crown/core/jobs/GameGetter.java
@@ -3,30 +3,18 @@ package com.sosu.rest.crown.core.jobs;
 
 import com.sosu.rest.crown.core.service.ImageUploader;
 import com.sosu.rest.crown.entity.mongo.Category;
-import com.sosu.rest.crown.entity.postgres.Game;
 import com.sosu.rest.crown.repo.mongo.CategoryRepository;
 import com.sosu.rest.crown.repo.postgres.GameRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 @Component
 @DependsOn("imageUploader")
@@ -41,11 +29,6 @@ public class GameGetter {
 
     @Autowired
     private ImageUploader imageUploader;
-
-    private static final String GAME_GENRE = "5eebb7587ea2d9497f4d7634";
-    private static final String METACRITIC_COM = "https://www.metacritic.com";
-
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.ENGLISH);
 
     private static final List<String> LINKS = new ArrayList<>() {
         {
@@ -100,124 +83,21 @@ public class GameGetter {
         }
     };
 
-//    @Scheduled(cron = " 0 0 0 ? * * ")
+    @Scheduled(cron = " 0 0 0 ? * * ")
     private void startTask() {
         log.info("Started game update service");
         for (int i = 0; i < LINKS.size(); i++) {
             String consoleLink = LINKS.get(i);
             String category = CATEGORY_IDS.get(i);
-            run(consoleLink, generateMap(i), category);
-        }
-        log.info("Finished game update service");
-    }
-
-    private Map<String, String> generateMap(Integer index) {
-        HashMap<String, String> genres = new HashMap<>();
-        String parentId = CATEGORY_IDS.get(index);
-        List<Category> categoryList = categoryRepository.findByParentId(parentId, Sort.by(Sort.Direction.ASC, "name"));
-        for (int i = 0; i < categoryList.size(); i++) {
-            genres.put(CATEGORIES.get(i), categoryList.get(i).getDefaultCategory());
-        }
-        return genres;
-    }
-
-    public void run(String consoleLink, Map<String, String> genre, String category) {
-        String selected = consoleLink;
-        boolean error = false;
-        while (!error) {
-            try {
-                Document document = Jsoup.connect(selected).get();
-                Elements link = document.getElementsByClass("clamp-list");
-                for (Element item : link) {
-                    for (Element gameItem : item.getElementsByTag("tr")) {
-                        String imageUrl = "https://2img.net/i/default.png";
-                        if (CollectionUtils.isNotEmpty(gameItem.getElementsByClass("clamp-image-wrap"))) {
-                            imageUrl = gameItem.getElementsByClass("clamp-image-wrap").get(0).getElementsByTag("a").get(0)
-                                    .getElementsByTag("img").get(0).attr("src");
-                        }
-                        if (CollectionUtils.isNotEmpty(gameItem.getElementsByClass("clamp-summary-wrap"))) {
-                            Elements detail = gameItem.getElementsByClass("clamp-summary-wrap").get(0).getElementsByTag("a");
-                            String linkOfGame = detail.get(1).attr("href");
-                            String summary = gameItem.getElementsByClass("summary").get(0).html();
-                            String gameName = detail.get(1).getElementsByTag("h3").html();
-                            Document documentGame = Jsoup.connect(METACRITIC_COM + linkOfGame).get();
-                            if (gameRepository.findByName(gameName) != null) {
-                                Game game = gameRepository.findByName(gameName);
-                                if (!game.getCategoryId().contains(category)) {
-                                    game.setCategoryId(game.getCategoryId() + ";" + category);
-                                    gameRepository.save(game);
-                                }
-                            } else {
-                                Game game = new Game();
-                                game.setName(gameName);
-                                game.setMainCategoryId(GAME_GENRE);
-                                game.setConsoleCategoryId(category);
-                                game.setDescription(summary);
-                                gameDetail(documentGame, genre, game, imageUrl);
-                            }
-                        }
-                    }
-                }
-                Element next = document.getElementsByClass("flipper next").get(0);
-                Elements actions = next.getElementsByClass("action");
-                if (CollectionUtils.isNotEmpty(actions) && StringUtils.isNotBlank(actions.get(0).attr("href"))) {
-                    selected = METACRITIC_COM + actions.get(0).attr("href");
-                } else {
-                    error = true;
-                }
-            } catch (Exception e) {
-                log.error("Error getting game: {}", consoleLink);
+            HashMap<String, String> genres = new HashMap<>();
+            String parentId = CATEGORY_IDS.get(i);
+            List<Category> categoryList = categoryRepository.findByParentId(parentId, Sort.by(Sort.Direction.ASC, "name"));
+            for (int j = 0; j < categoryList.size(); j++) {
+                genres.put(CATEGORIES.get(j), categoryList.get(j).getDefaultCategory());
             }
+            (new GameGetterThread(gameRepository, imageUploader, consoleLink, category, genres)).start();
         }
     }
 
-    public void gameDetail(Document documentGame, Map<String, String> genres, Game game, String imageUrl) {
-        StringBuilder platforms = new StringBuilder();
-        String publisher = null;
-        if (CollectionUtils.isNotEmpty(documentGame.getElementsByClass("summary_detail publisher"))) {
-            publisher = documentGame.getElementsByClass("summary_detail publisher").get(0).getElementsByClass("data")
-                    .get(0).getElementsByTag("a").html();
-        }
-        documentGame.getElementsByClass("summary_details");
-        String developer = null;
-        if (CollectionUtils.isNotEmpty(documentGame.getElementsByClass("summary_detail developer"))) {
-            developer = documentGame.getElementsByClass("summary_detail developer").get(0).getElementsByClass("data")
-                    .html();
-        }
-        if (StringUtils.isNotBlank(developer)) {
-            developer = publisher;
-        }
-        if (StringUtils.isNotBlank(publisher)) {
-            publisher = developer;
-        }
-        String releaseDate = documentGame.getElementsByClass("summary_detail release_data").get(0).getElementsByClass("data")
-                .html();
-        LocalDate formattedDate = LocalDate.parse(releaseDate, FORMATTER);
 
-        for (Element element : documentGame.getElementsByClass("summary_detail product_genre").get(0).getElementsByClass("data")) {
-            if (genres.containsKey(element.html())) {
-                platforms.append(genres.get(element.html()));
-                platforms.append(";");
-            }
-        }
-        if (StringUtils.isBlank(platforms.toString())) {
-            platforms.append(genres.get("General"));
-        } else {
-            platforms.deleteCharAt(platforms.length() - 1);
-        }
-        String genre = platforms.toString();
-
-        String rating = "E";
-        if (CollectionUtils.isNotEmpty(documentGame.getElementsByClass("summary_detail product_rating"))) {
-            rating = documentGame.getElementsByClass("summary_detail product_rating").get(0).getElementsByClass("data")
-                    .html();
-        }
-        game.setCert(rating);
-        game.setDeveloper(developer);
-        game.setPublisher(publisher);
-        game.setPublishDate(formattedDate);
-        game.setCategoryId(genre);
-        game.setImage(imageUploader.uploadImage(imageUrl, game.getName()));
-        gameRepository.save(game);
-    }
 }
---
+++ src/main/java/com/sosu/rest/crown/core/jobs/GameGetterThread.java
@@ -0,0 +1,148 @@
+package com.sosu.rest.crown.core.jobs;
+
+import com.sosu.rest.crown.core.service.ImageUploader;
+import com.sosu.rest.crown.entity.postgres.Game;
+import com.sosu.rest.crown.repo.postgres.GameRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Map;
+
+@Slf4j
+public class GameGetterThread extends Thread {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.ENGLISH);
+    private static final String GAME_GENRE = "5eebb7587ea2d9497f4d7634";
+    private static final String METACRITIC_COM = "https://www.metacritic.com";
+    private final GameRepository gameRepository;
+    private final ImageUploader imageUploader;
+    private final String consoleLink;
+    private final String category;
+    private final Map<String, String> genre;
+
+    protected GameGetterThread(GameRepository gameRepository, ImageUploader imageUploader, String consoleLink, String category, Map<String, String> genre) {
+        this.gameRepository = gameRepository;
+        this.imageUploader = imageUploader;
+        this.consoleLink = consoleLink;
+        this.category = category;
+        this.genre = genre;
+    }
+
+    public void run() {
+        getGame();
+    }
+
+    private void getGame() {
+        String selected = consoleLink;
+        boolean error = false;
+        while (!error) {
+            try {
+                Document document = Jsoup.connect(selected).get();
+                Elements link = document.getElementsByClass("clamp-list");
+                for (Element item : link) {
+                    for (Element gameItem : item.getElementsByTag("tr")) {
+                        String imageUrl = "https://2img.net/i/default.png";
+                        if (CollectionUtils.isNotEmpty(gameItem.getElementsByClass("clamp-image-wrap"))) {
+                            imageUrl = gameItem.getElementsByClass("clamp-image-wrap").get(0).getElementsByTag("a").get(0)
+                                    .getElementsByTag("img").get(0).attr("src");
+                        }
+                        if (CollectionUtils.isNotEmpty(gameItem.getElementsByClass("clamp-summary-wrap"))) {
+                            Elements detail = gameItem.getElementsByClass("clamp-summary-wrap").get(0).getElementsByTag("a");
+                            String linkOfGame = detail.get(1).attr("href");
+                            String summary = gameItem.getElementsByClass("summary").get(0).html();
+                            String gameName = detail.get(1).getElementsByTag("h3").html();
+                            Document documentGame = Jsoup.connect(METACRITIC_COM + linkOfGame).get();
+                            if (gameRepository.findByName(gameName) != null) {
+                                Game game = gameRepository.findByName(gameName);
+                                if (!game.getCategoryId().contains(category)) {
+                                    game.setCategoryId(game.getCategoryId() + ";" + category);
+                                    gameRepository.save(game);
+                                }
+                            } else {
+                                Game game = new Game();
+                                game.setName(gameName);
+                                game.setMainCategoryId(GAME_GENRE);
+                                game.setConsoleCategoryId(category);
+                                game.setDescription(summary);
+                                gameDetail(documentGame, genre, game, imageUrl);
+                            }
+                        }
+                    }
+                }
+                if (!CollectionUtils.isEmpty(document.getElementsByClass("flipper next"))) {
+                    Element next = document.getElementsByClass("flipper next").get(0);
+                    Elements actions = next.getElementsByClass("action");
+                    if (CollectionUtils.isNotEmpty(actions) && StringUtils.isNotBlank(actions.get(0).attr("href"))) {
+                        selected = METACRITIC_COM + actions.get(0).attr("href");
+                    } else {
+                        error = true;
+                    }
+                } else {
+                    error = true;
+                }
+            } catch (Exception e) {
+                log.error("Error getting game: {} {}", consoleLink, ExceptionUtils.getStackTrace(e));
+            }
+        }
+        log.info("Finished game update service for {}", consoleLink);
+    }
+
+    private void gameDetail(Document documentGame, Map<String, String> genres, Game game, String imageUrl) {
+        StringBuilder platforms = new StringBuilder();
+        String publisher = null;
+        if (CollectionUtils.isNotEmpty(documentGame.getElementsByClass("summary_detail publisher"))) {
+            publisher = documentGame.getElementsByClass("summary_detail publisher").get(0).getElementsByClass("data")
+                    .get(0).getElementsByTag("a").html();
+        }
+        documentGame.getElementsByClass("summary_details");
+        String developer = null;
+        if (CollectionUtils.isNotEmpty(documentGame.getElementsByClass("summary_detail developer"))) {
+            developer = documentGame.getElementsByClass("summary_detail developer").get(0).getElementsByClass("data")
+                    .html();
+        }
+        if (StringUtils.isNotBlank(developer)) {
+            developer = publisher;
+        }
+        if (StringUtils.isNotBlank(publisher)) {
+            publisher = developer;
+        }
+        String releaseDate = documentGame.getElementsByClass("summary_detail release_data").get(0).getElementsByClass("data")
+                .html();
+        LocalDate formattedDate = LocalDate.parse(releaseDate, FORMATTER);
+
+        for (Element element : documentGame.getElementsByClass("summary_detail product_genre").get(0).getElementsByClass("data")) {
+            if (genres.containsKey(element.html())) {
+                platforms.append(genres.get(element.html()));
+                platforms.append(";");
+            }
+        }
+        if (StringUtils.isBlank(platforms.toString())) {
+            platforms.append(genres.get("General"));
+        } else {
+            platforms.deleteCharAt(platforms.length() - 1);
+        }
+        String genre = platforms.toString();
+
+        String rating = "E";
+        if (CollectionUtils.isNotEmpty(documentGame.getElementsByClass("summary_detail product_rating"))) {
+            rating = documentGame.getElementsByClass("summary_detail product_rating").get(0).getElementsByClass("data")
+                    .html();
+        }
+        game.setCert(rating);
+        game.setDeveloper(developer);
+        game.setPublisher(publisher);
+        game.setPublishDate(formattedDate);
+        game.setCategoryId(genre);
+        game.setImage(imageUploader.uploadImage(imageUrl, game.getName()));
+        gameRepository.save(game);
+    }
+}
